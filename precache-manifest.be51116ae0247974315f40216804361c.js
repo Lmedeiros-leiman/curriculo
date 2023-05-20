84af9cad94101dff82a7b86e22cbf54882771d2c
Leonardo <lmedeiros045@gmail.com>
2023-05-20 20:40:08 -0300

    Updates

---
+++ precache-manifest.be51116ae0247974315f40216804361c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "793d741028e0b0ab23ed",
+    "url": "/curriculo/static/css/main.967a3cbe.chunk.css"
+  },
+  {
+    "revision": "793d741028e0b0ab23ed",
+    "url": "/curriculo/static/js/main.793d7410.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/curriculo/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/curriculo/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "2034c02b2988c3c1a086",
+    "url": "/curriculo/static/js/runtime~main.2034c02b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/curriculo/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d7f55a332e1a170a01144754f34fb580",
+    "url": "/curriculo/index.html"
+  }
+];
