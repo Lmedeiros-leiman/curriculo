41cd85e1bfa8e27b6901500ecb3f48ee9675f31b
Leonardo <lmedeiros045@gmail.com>
2024-05-09 10:35:11 -0300

    Recomeçando projeto sem toda essa complexidade desnecessária

--- public/build/assets/sobremim-1c749da6.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-8d86a73b.js";const r=()=>o.jsx("div",{}),s=r;export{s as default};
--- resources/js/Pages/Welcome/sobremim.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const SobreMim = () => {
-    return(
-    <div>
-
-    </div>)
-}
-export default SobreMim
