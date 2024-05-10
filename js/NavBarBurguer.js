12e5c2dd5e0bc3578d8e556da0be907ae094e3c0
Leonardo <lmedeiros045@gmail.com>
2024-05-09 21:31:32 -0300

    poucos detalhes extras e quase finalizando navbar

--- js/NavBarBurguer.js
+++ js/NavBarBurguer.js
@@ -7,7 +7,6 @@ const NavBarBurguer = ( () => {
         
         const firstBar = BurguerButton.children[0]
         const secondBar = BurguerButton.children[1]
-        const thirdBar = BurguerButton.children[2]
         
 
         if (BurguerButton.getAttribute("state") === "open") 
@@ -20,6 +19,8 @@ const NavBarBurguer = ( () => {
                 secondBar.style.transform = "translateY(-50%)"
 
                 SideBar.setAttribute("status","hidden")
+                SideBar.style.transform = "translateY(0%)"
+                
             } 
         else {
                 BurguerButton.setAttribute("state","open")
@@ -30,6 +31,9 @@ const NavBarBurguer = ( () => {
                 secondBar.style.top = "25%"
                 secondBar.style.transform = "translateY(100%) rotate(45deg)"
                 SideBar.setAttribute("status","visible")
+
+                SideBar.style.transform = "translateY(100%)"
+                
 
 
         } 
